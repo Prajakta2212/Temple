41e7a192ae3842b70ea9710270cf9360e16dcf11
Prajakta2212 <99804726+Prajakta2212@users.noreply.github.com>
2026-04-11 17:20:03 +0530

    Format LoginSuccess and SignupSuccess classes

--- lib/bloc/loginbloc.dart
+++ lib/bloc/loginbloc.dart
@@ -28,9 +28,9 @@ class AuthInitial extends AuthState {}
 
 class AuthLoading extends AuthState {}
 
-class LoginSuccess extends AuthState {}     // ✅ login only
+class LoginSuccess extends AuthState {}    
 
-class SignupSuccess extends AuthState {}    // ✅ signup only
+class SignupSuccess extends AuthState {}   
 
 class AuthError extends AuthState {
   final String message;
